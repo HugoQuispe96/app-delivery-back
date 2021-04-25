bb1913eac89c66a97cbe24de7418bcd25fd29a4d
BrotherHugo <hugo.quispe96@outlook.es>
2021-04-24 23:00:13 -0400

    backend v1.1

--- utiles/nodemailder.js
+++ utiles/nodemailder.js
@@ -6,11 +6,10 @@ import { dirname } from 'path';
 
 const __dirname = dirname(__filename);
 
-const { EMAIL_USER, EMAIL_PASS, EMAIL_HOST, EMAIL_PORT, EMAIL_SECURE, EMAIL_FROM, URL_FRONTEND } = process.env;
+const { EMAIL_USER, EMAIL_PASS, EMAIL_HOST, EMAIL_PORT, EMAIL_FROM, URL_FRONTEND } = process.env;
 const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
   port: EMAIL_PORT,
-  secure: EMAIL_SECURE,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS
