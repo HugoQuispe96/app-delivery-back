e67e4357eeabab63948a8f42851f2ff9c73a9cc8
BrotherHugo <hugo.quispe96@outlook.es>
2021-04-24 22:09:02 -0400

    backend v1

---
+++ databaseLogic/restaurant/logic.js
@@ -0,0 +1,35 @@
+import Restaurant from './scheme.js';
+import Category from '../categoria/scheme.js';
+import Product from '../producto/scheme.js';
+
+const findOneForID = async (id) => {
+  let response = await Restaurant.findByPk(id);
+  return response;
+}
+
+const updateOne = async (id, data) => {
+  let response = await Restaurant.update(
+    data,
+    {
+      where: {
+        id
+      }
+    }
+  );
+  return response;
+}
+
+const findOneForIDCompplete = async (id) => {
+  let response = await Restaurant.findByPk(id, {
+    include: {
+      model: Category,
+      include: Product
+    }
+  });
+  return response;
+}
+export {
+  updateOne,
+  findOneForID,
+  findOneForIDCompplete
+}
